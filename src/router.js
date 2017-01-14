3b18af9fc51bc49be78f1233622815d21486f220
Dane <dingchao.lonton@gmail.com>
2017-01-14 20:44:14 +0800

    add normalize.css and adjust the index route position

--- src/router.js
+++ src/router.js
@@ -8,8 +8,8 @@ import Projects from './components/mainbody/projects';
 export default function(props) {
     return <Router history={hashHistory}>
             <Route path="/" component={App} >
-                <IndexRoute component={Projects}/>
                 <Route path="*" component={MainBody}/>
+                <IndexRoute component={Projects}/>
             </Route>
            </Router>
 }
