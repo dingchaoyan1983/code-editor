3ae23380e0ee5480b843825c0ddd678d448fc5e8
Dane <dingchao.lonton@gmail.com>
2017-01-17 21:22:14 +0800

    edit file

--- src/components/mainbody/code-editor.js
+++ src/components/mainbody/code-editor.js
@@ -2,14 +2,58 @@ import React from 'react';
 import ReactCodemirror from 'react-codemirror';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/addon/scroll/simplescrollbars.js';
+import 'codemirror/addon/scroll/simplescrollbars.css';
+import Col from 'react-bootstrap/lib/Col';
+import Button from 'react-bootstrap/lib/Button';
+import classname from 'classname';
+
+const MODIFY= '修改';
+const SAVE = '保存';
+const { PureComponent } = React;
 
 const options = {
     lineNumbers: true,
-	readOnly: true,
-	mode: 'javascript'
+	mode: 'javascript',
+    scrollbarStyle: 'simple'
 }
 
-export default function(props) {
-    const content = props.file.get('content');
-    return <ReactCodemirror value={content} options={options}/>
+export default class extends PureComponent {
+    constructor(props, context) {
+        super(props, context);
+
+        this.options = {
+            lineNumbers: true,
+            mode: 'javascript',
+            scrollbarStyle: 'simple'
+        };
+
+        this.state = {
+            readOnly: true
+        }
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    render() {
+        const content = this.props.file.get('content');
+        this.options.readOnly = this.state.readOnly;
+
+        return <div>
+                  <div className="clearfix editor-toolbar__wrapper">
+                      <Col sm={3} smOffset={9} className="editor-toolbar">
+                        <Button bsSize="xsmall" bsStyle="success" onClick={this.onClick}>{this.state.readOnly ? MODIFY : SAVE}</Button>
+                      </Col>
+                  </div>
+                  <ReactCodemirror value={content} options={this.options} className={classname(this.state.readOnly ? 'readonly' : '')}/>
+               </div>
+    }
+
+    onClick() {
+        let state = !this.state.readOnly;
+        this.setState({
+            readOnly: state
+        });
+    }
 }
+
